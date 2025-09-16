77ba7390f2d2c3aaf2e0f2f34672344c289ffb48
Axalon01 <versalife2@gmail.com>
2025-09-15 21:40:23 -0400

    Add GameOver checking to GameManager; Add several methods to Form1.cs
    - Changed OriginalVolume in GameManager to SetVolume and made it a public property
    - Removed ReducedVolume
    - Added PausedVolume property to set pause volume to 10% or lower depending on user settings
    - Turned IsPaused const into a public property
    Added CheckGameOver and GameOver methods
    - Created InitializeCustomComponents method to Form1 to initialize key up, down, and preview
    - Created OnKeyDown and KeyIsUp methods to control padding movement
    - Created TogglePause method to handle pausing and unpausing

--- BreakoutCloneSolution/BreakoutClone/Form1.cs
+++ BreakoutCloneSolution/BreakoutClone/Form1.cs
@@ -3,6 +3,7 @@ namespace BreakoutClone
 {
 	public partial class Form1 : Form
 	{
+		private GameManager gameManager; //Reference to the GameManager class
 		public Form1()
 		{
 			InitializeComponent();
@@ -12,12 +13,65 @@ namespace BreakoutClone
 
 		private void SetInitialWindowSizeAndPosition()
 		{
-			throw new NotImplementedException();
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.ClientSize = new Size(800, 600);
+
+			gameManager = new GameManager(); //Create the game manager object
 		}
 
 		private void InitializeCustomComponents()
 		{
-			throw new NotImplementedException();
+			this.KeyDown += new KeyEventHandler(OnKeyDown);
+			this.KeyUp += new KeyEventHandler(KeyIsUp);
+			this.KeyPreview = true; // Ensures the form receives key events
+		}
+
+		private void OnKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape || e.KeyCode == Keys.Space)
+			{
+				TogglePause();
+			}
+			else if (e.KeyCode == Keys.Left)
+			{
+				gameManager.goLeft = true;
+			}
+			else if ( e.KeyCode == Keys.Right)
+			{
+				gameManager.goRight = true;
+			}
+		}
+
+		// Key up event to stop paddle
+		private void KeyIsUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Down)
+			{
+				gameManager.goLeft = false;
+			}
+			else if (e.KeyCode == Keys.Up)
+			{
+				gameManager.goRight = false;
+			}
+		}
+
+		private void TogglePause()
+		{
+			gameManager.IsPaused = !gameManager.IsPaused;
+
+			if (gameManager.IsPaused)
+			{
+				GameTimer.Stop();
+				waveOutEvent.Volume = gameManager.PausedVolume;
+			}
+			else
+			{
+				GameTimer.Start();
+				waveOutEvent.Volume = gameManager.SetVolume;
+			}
+
+			this.Invalidate(); //Force the form to repaint
 		}
 	}
 }
--- BreakoutCloneSolution/BreakoutClone/GameManager.cs
+++ BreakoutCloneSolution/BreakoutClone/GameManager.cs
@@ -10,10 +10,38 @@ namespace BreakoutClone
 	{
 		private int lives = 3;
 		private int score = 0;
-		private const float reducedVolume = 0.1f; //10% volume
-		private float originalVolume = 0.7f;
-		private bool isPaused = false;
+		public float SetVolume { get; set; } = 0.7f; // Can be changed later, like by the user
+		public float PausedVolume
+		{
+			get
+			{
+				return SetVolume >= 0.1f ? 0.1f : SetVolume;
+			}
+		}
+		public bool IsPaused { get; set; } = false;
 		private bool showPauseOverplay = false;
+		public bool goLeft, goRight;
 		int playerSpeed = 12; //Sets default player speed
+
+		private void CheckGameOver()
+		{
+			if (lives == 0)
+			{
+				GameOver("Out of lives. Better luck nenxt time.");
+			}
+			else if (score == 864)
+			{
+				GameOver("Congrats! You bashed all those bricks!");
+			}
+		}
+
+		private void GameOver(string message)
+		{
+			GameTimer.Stop();
+			MessageBox.Show(message);
+			ResetGameState();
+			GameTimer.Start();
+			Application.Exit();
+		}
 	}
 }
